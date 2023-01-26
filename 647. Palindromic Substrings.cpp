569880cf5f4c5d6f1316b303587ae687e07fb6a2
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2023-01-27 02:19:40 +0530

    LC

---
+++ 647. Palindromic Substrings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int ans = 0;
+        int n = s.length();
+        vector<vector<bool>> dp(n, vector<bool> (n, false));
+
+        for(int gap=0;gap<n;gap++){
+            for(int i=0, j=gap;j<n;i++, j++){
+                if(gap == 0)
+                    dp[i][j] = true;
+                
+                else if(gap == 1)
+                    dp[i][j] = (s[i] == s[j]);
+                
+                else if(s[i] == s[j])
+                    dp[i][j] = dp[i+1][j-1];
+                
+                if(dp[i][j])
+                    ans++;
+            }
+        }
+        return ans;
+    }
+};
