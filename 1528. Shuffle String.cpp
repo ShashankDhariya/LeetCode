cbc903f14a1fb90d55f841e76fed0f99d48e875a
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2023-02-28 20:02:21 +0530

    LC

---
+++ 1528. Shuffle String.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        int n = s.length();
+        string ans;
+        ans.resize(n);
+
+        for(int i=0;i<n;i++)
+            ans[indices[i]] = s[i];
+        return ans;
+    }
+};
