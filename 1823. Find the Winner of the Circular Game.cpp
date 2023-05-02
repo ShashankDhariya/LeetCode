881cad4e314d5796888c82f12e5bea34b10984dd
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2023-05-02 23:34:28 +0530

    LC

---
+++ 1823. Find the Winner of the Circular Game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findTheWinner(int n, int k) {
+        queue<int> q;
+        for(int i=0;i<n;i++)
+            q.push(i+1);
+
+        while(q.size() > 1){
+            for(int i=0;i<k-1;i++){
+                int num = q.front();
+                q.pop();
+                q.push(num);
+            }
+            q.pop();
+            cout<<q.front()<<" ";
+        }
+        return q.front();
+    }
+};
