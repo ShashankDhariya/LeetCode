e936475871f2f72dbfb9c642fe5b7319a9a80968
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2023-07-30 16:34:11 +0530

    LC

---
+++ 2798. Number of Employees Who Met the Target.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int ans = 0;
+        
+        for(auto h: hours){
+            if(h >= target)
+                ans++;
+        }
+        
+        return ans;
+    }
+};
