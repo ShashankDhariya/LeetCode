78d211669edd2894eac83399f84fc6071908c5cc
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2023-08-16 23:57:04 +0530

    LC

--- 239. Sliding Window Maximum.cpp
+++ 239. Sliding Window Maximum.cpp
@@ -1,3 +1,4 @@
+// Using Deque
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
@@ -16,6 +17,31 @@ public:
             if(i >= k-1)    ans.push_back(nums[dq.front()]);
         }
 
+        return ans;
+    }
+};
+
+OR
+
+// Using hashmap
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> ans;
+
+        map<int, int> freq;
+        for(int i=0;i<k;i++)
+            freq[nums[i]]++;
+        ans.push_back(freq.rbegin()->first);
+
+        for(int i=k;i<n;i++){
+            freq[nums[i]]++;
+            freq[nums[i - k]]--;
+            if(freq[nums[i-k]] == 0)
+                freq.erase(nums[i-k]);
+            ans.push_back(freq.rbegin()->first);
+        }
         return ans;
     }
 };
