17786bfb1d99b3e1c00414b07ba2cded034b60fc
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2023-08-19 00:02:08 +0530

    LC

--- 231. Power of Two.cpp
+++ 231. Power of Two.cpp
@@ -14,3 +14,12 @@ public:
         return 1;
     }
 };
+
+// O(1)
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n <= 0)  return 0;
+        return (n & -n) == n;
+    }
+};
