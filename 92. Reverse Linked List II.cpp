bfdf61a5499365449bd14ea56993ae283b0041ed
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2023-09-07 23:13:52 +0530

    LC

--- 92. Reverse Linked List II.cpp
+++ 92. Reverse Linked List II.cpp
@@ -10,47 +10,22 @@
  */
 class Solution {
 public:
-    
-    ListNode* reverse(ListNode* head){
-        ListNode* p = NULL;
-        ListNode* nxt = NULL;
-        ListNode* ptr = head;
-        while(ptr){
-            nxt = ptr->next;
-            ptr->next = p;
-            p = ptr;
-            ptr = nxt;
-        }
-        return p;
-    }
-    
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        ListNode* pre = new ListNode(0);
-        pre->next = head;
-        ListNode* ptr1 = pre;
-        int i=0;
-        
-        if(left == right)
-            return head;
-        
-        while(ptr1->next && i+1 != left){
-           ptr1 = ptr1->next;
-           i++;
+        ListNode* temp = new ListNode(0);
+        temp->next = head;
+        ListNode* prev = temp;
+
+        for(int i=0;i<left-1;i++)
+            prev = prev->next;
+
+        ListNode* ptr = prev->next;
+        for(int i=0;i<right-left;i++){
+            ListNode* nxt = ptr->next;
+            ptr->next = nxt->next;
+            nxt->next = prev->next;
+            prev->next = nxt;
         }
-        ListNode* ptr2 = ptr1;
-        while(ptr2 && i != right){
-            ptr2 = ptr2->next;
-            i++;
-        }
-        ListNode* nxt = ptr2->next;
-            ptr2->next = NULL;
-    
-        ListNode* h = reverse(ptr1->next);
-        ptr1->next = h;
-        while(h->next)
-            h = h->next;
-        h->next = nxt;
-        
-        return pre->next;
+
+        return temp->next;
     }
 };
