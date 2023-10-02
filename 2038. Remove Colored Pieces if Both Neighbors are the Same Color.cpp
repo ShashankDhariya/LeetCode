8053680aa1bf349df48029eb27f9b39646f0594b
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2023-10-02 23:29:31 +0530

    LC

--- 2038. Remove Colored Pieces if Both Neighbors are the Same Color.cpp
+++ 2038. Remove Colored Pieces if Both Neighbors are the Same Color.cpp
@@ -1,34 +1,16 @@
 class Solution {
 public:
-
-    int moves(string colors, char take, char noTake){
-        int res = -1;
-        int i = 0;
-        int n = colors.size();
-
-        int ans = 0;
-        for(int j=0;j<n;j++){
-            while(i < j && colors[i] != take)
-                i++;
-
-            if(colors[j] == noTake){
-                res = j - i - 2;
-                ans += res > 0? res: 0;
-                i = j;
-            }
-
-            else if(j == n-1){
-                res = j - i - 1;
-                ans += (res > 0)? res: 0;
-            }
-        }
-
-        return ans;
-    }
-
     bool winnerOfGame(string colors) {
-        int alice = moves(colors, 'A', 'B');
-        int bob = moves(colors, 'B', 'A');
+        int alice = 0;
+        int bob = 0;
+
+        int n = colors.length();
+        for(int i=1;i<n-1;i++){
+            if(colors[i-1] == 'A' && colors[i] == 'A' && colors[i+1] == 'A')
+                alice++;
+            else if(colors[i-1] == 'B' && colors[i] == 'B' && colors[i+1] == 'B')
+                bob++;
+        }
 
         return alice > bob;
     }
