76806ee217ae16b6fa9ce291e35098c10fe272f1
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2023-10-16 16:29:11 +0530

    LC

--- 119. Pascal's Triangle II.cpp
+++ 119. Pascal's Triangle II.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<vector<int>> res(rowIndex+1);
-
-        for(int i=0;i<=rowIndex;i++){
-            res[i].resize(i+1, 1);
+        if(rowIndex == 0)
+            return {1};
+            
+        vector<int> prev(2, 1);
+        for(int i=2;i<=rowIndex;i++){
+            vector<int> curr(i+1, 1);
             for(int j=1;j<i;j++)
-                res[i][j] = res[i-1][j-1] + res[i-1][j];
+                curr[j] = prev[j-1] + prev[j];
+            prev = curr;
         }
-        return res[rowIndex];
+        return prev;
     }
 };
