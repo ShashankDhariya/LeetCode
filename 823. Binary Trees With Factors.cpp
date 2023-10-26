2fef3b22fddff7b9bba2acd3cf404d5b1072f380
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2023-10-27 01:19:00 +0530

    LC

--- 823. Binary Trees With Factors.cpp
+++ 823. Binary Trees With Factors.cpp
@@ -32,3 +32,28 @@ public:
         return ans%MOD;
     }
 };
+
+/*
+OR
+class Solution {
+public:
+    int mod = 1e9+7;
+    int numFactoredBinaryTrees(vector<int>& arr) {
+        long long ans = 0;
+        sort(arr.begin(), arr.end());
+
+        unordered_map<int, long long> dp;
+        int n = arr.size();
+        for(int i=0;i<n;i++){
+            dp[arr[i]] = 1;
+            for(int j=0;j<i;j++){
+                if(arr[i] % arr[j] == 0)
+                    dp[arr[i]] = (dp[arr[i]] + dp[arr[j]] * dp[arr[i] / arr[j]]) % mod;
+            }
+            ans = (ans + dp[arr[i]]) % mod;
+        }
+
+        return ans;
+    }
+};
+*/
