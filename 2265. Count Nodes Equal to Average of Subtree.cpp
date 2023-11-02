8cb0f7c39e112c867daaf9058b98b1a9e65121b5
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2023-11-02 13:18:51 +0530

    LC

--- 2265. Count Nodes Equal to Average of Subtree.cpp
+++ 2265. Count Nodes Equal to Average of Subtree.cpp
@@ -9,27 +9,29 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
 
-    pair<int, int> traverse(TreeNode* root, int& ans, int& n){
-        if(!root)
-            return {0, 0};
+    pair<int, int> traverse(TreeNode* root, int& ans){
+    if(!root)
+        return {0, 0};
 
-        n++;
-        auto left = traverse(root->left, ans, n);
-        auto right = traverse(root->right, ans, n);
+    auto left = traverse(root->left, ans);
+    auto right = traverse(root->right, ans);
+    
+    int sum = root->val + left.first + right.first;
+    int n = 1 + left.second + right.second;
 
-        if((left.first + right.first + root->val)/(left.second + right.second + 1) == root->val)
-            ans++;
-        cout<<left.first + right.first + root->val<<" ";
-        return {root->val + left.first + right.first, left.second + right.second + 1};
-    }
+    if(sum / n == root->val)
+        ans++;
+
+    return {sum, n};
+}
 
     int averageOfSubtree(TreeNode* root) {
         int ans = 0;
-        int n = 0;
-        traverse(root, ans, n);
+        traverse(root, ans);
         return ans;
     }
 };
