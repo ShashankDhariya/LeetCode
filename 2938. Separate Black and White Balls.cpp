35f720c1b28e47cf9e71f4f46cc70310a2af7921
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2023-11-20 23:24:17 +0530

    LC

---
+++ 2938. Separate Black and White Balls.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long ans = 0;
+        
+        int idx = 0;
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            if(s[i] == '0')
+                ans += (i - idx++);
+        }
+        
+        return ans;
+    }
+};
