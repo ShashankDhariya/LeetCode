9672b2f2f2c58c2052a101b1860c7c0c3887f870
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2023-11-29 18:35:12 +0530

    LC

--- 191. Number of 1 Bits.cpp
+++ 191. Number of 1 Bits.cpp
@@ -1,40 +1,12 @@
-// Using built in function '__builtin_popcount()'
-
-class Solution {
-public:
-    int hammingWeight(uint32_t n) {
-        return __builtin_popcount(n);
-    }
-};
-
-//   OR - Using normal looping 
-/*
-  class Solution {
-public:
-    int hammingWeight(uint32_t n) {
-        int ans = 0;
-        while(n > 0){
-            if(n % 2 == 1)
-                ans++;
-            n /= 2;
-        }
-        return ans;
-    }
-};
-*/
-
-//   OR - Bit Manipulation 
-/*
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int ans = 0;
-        while(n > 0){
-            if(n & 1 > 0)
-                ans++;
+
+        while(n){
+            ans += (n & 1);
             n >>= 1;
         }
         return ans;
     }
 };
-*/
