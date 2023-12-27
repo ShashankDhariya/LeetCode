01bff3323511a334f0d13a24706e6e89adb754eb
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2023-12-27 23:06:13 +0530

    LC

--- 1578. Minimum Time to Make Rope Colorful.cpp
+++ 1578. Minimum Time to Make Rope Colorful.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
-        int n = neededTime.size();
         int ans = 0;
-        int time = neededTime[0];
-        int m = neededTime[0];
-        for(int i=1;i<n;i++){
-            if(colors[i] != colors[i-1]){
-                ans += (time - m);
-                time = 0;
-                m = 0;
+
+        int i = 0;
+        int n = colors.length();
+        while(i < n){
+            int time = -1;
+            int sum = 0;
+            int j = i;
+            while(j < n && colors[j] == colors[i]){
+                sum += neededTime[j];
+                time =  max(time, neededTime[j]);
+                j++;
             }
-            time += neededTime[i];
-            m = max(m,neededTime[i]);
+
+            if(j - i > 1)
+                ans +=  (sum-time);
+            
+            i = j;
         }
-        ans += (time - m);
-    return ans;
+
+        return ans;
     }
 };
