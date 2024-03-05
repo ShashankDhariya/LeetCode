812d5e8feb090394af8c258ede156632665a0207
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2024-03-06 00:21:38 +0530

    LC

---
+++ 1750. Minimum Length of String After Deleting Similar Ends.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        int i = 0;
+        int j = s.length()-1;
+        while(i < j){
+            if(s[i] != s[j])
+                break;
+
+            while(i < j && s[i] == s[i+1])
+                i++;
+
+            while(i <= j && s[j] == s[j-1])
+                j--;
+
+            i++;
+            j--;
+        }
+
+        return max(0, j - i + 1);
+    }
+};
