1cf6adf6b8544d17ea2b027eda461df828ef5a01
ShashankDhariya <92181600+ShashankDhariya@users.noreply.github.com>
2024-07-06 23:08:43 +0530

    LC

---
+++ 2582. Pass the Pillow.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        if((time / (n - 1)) % 2)
+            return n - (time % (n - 1));
+
+        return 1 + (time % (n - 1));
+    }
+};
